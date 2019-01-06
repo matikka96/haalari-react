2fbe15e96fb2778c2ab6177b56d8819ea3722b60
kasukosu <kazu.kozu@hotmail.com>
2019-01-06 19:13:36 +0200

    pieniä muutoksia img src

--- front/src/Components/post.jsx
+++ front/src/Components/post.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Config from '../config';
+
+const BACKURL = Config.URL.express;
 
 class Post extends Component {
     render() { 
         console.log(this.props);
+        const imageurl = BACKURL+'/'+this.props.post.postImg;
         return (       
             <div className="container col-md-6 col-md-offset-3">
                 <h3 className="">{this.props.post.postTitle}</h3>
-                <img src={this.props.post.picture} 
+                <img src={imageurl} 
                     onClick={() => this.props.onOpenPost(this.props.post)}
                     className="text-center" alt=""/>
                 <div>
