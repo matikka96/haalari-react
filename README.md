4cbb3895e8dd32b218636b5d9d61e99d74d3adb2
matikka96 <31724958+matikka96@users.noreply.github.com>
2019-01-09 16:25:29 +0200

    card layout implemented

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # haalari-react
+
+## Todo
+
+* duplicate check on vote request
+* open post seperately
+* Account view (username, avatar, sign up date etc...)
+* COMMENTS
--- front/src/Components/post.jsx
+++ front/src/Components/post.jsx
@@ -6,25 +6,24 @@ const BACKURL = Config.URL.express;
 class Post extends Component {
     render() { 
         const imageurl = BACKURL+'/'+this.props.post.postImg;
-        return (       
-            <div className="">
-                <h3 className="">{this.props.post.postTitle}</h3>
-                <img src={imageurl} 
-                    onClick={() => this.props.onOpenPost(this.props.post)}
-                    className="text-center img-fluid" alt="image"/>
-                <div>
-                    <p>
-                        {this.props.post.postDescription}
-                    </p>
-                    <button onClick={() => this.props.onVote(this.props.post._id)} 
-                        className="btn btn-primary btn-sm m-2">Like</button>
-                    {/* <span 
-                        className="badge badge-pill badge-primary m-2">
-                        Votes {this.props.post.votes.length}               
-                    </span> */}
-
-                </div>
-           </div>
+        return (
+            <div className="card mx-auto" style={{maxWidth: 400}}>
+              <img src={imageurl} 
+                onClick={() => this.props.onOpenPost(this.props.post)}
+                className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{this.props.post.postTitle}</h5>
+                <p className="card-text">{this.props.post.postDescription}</p>
+              </div>
+              <div className="card-body">
+                <button
+                    className="btn btn-secondary"
+                    onClick={() => this.props.onVote(this.props.post._id)}
+                    >Vote
+                </button>
+                <span class="badge badge-success m-2">{this.props.post.postVotes.length}</span>
+              </div>
+            </div>
         );
     }
 }
